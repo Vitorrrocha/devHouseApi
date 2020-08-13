97b9f1adaf49cf2993ad982d46d104d5675fafb6
vitorrrocha <vitorrochaflorip@hotmail.com>
2020-08-13 09:22:17 -0300

    Reserve Model and controller add

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,16 @@ import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 import uploadConfig from './config/upload';
 import DashboardController from './controllers/DashboardController';
+import ReserveController from './controllers/ReserveController';
 
 const routes = new Router();
 const upload = multer(uploadConfig);
 
-routes.get('/dashboard', DashboardController.show);
 
+// SESSIONS
 routes.post('/sessions', SessionController.store);
 
+// HOUSES
 routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 
 routes.get('/houses', HouseController.index);
@@ -19,6 +21,16 @@ routes.get('/houses', HouseController.index);
 routes.put('/houses/:house_id', upload.single('thumbnail') ,HouseController.update);
 
 routes.delete('/houses', HouseController.destroy);
+
+// DASHBOARD
+routes.get('/dashboard', DashboardController.show);
+
+// RESERVE
+routes.post('/houses/:house_id/reserve', ReserveController.store);
+
+
+
+
 
 
 export default routes;
