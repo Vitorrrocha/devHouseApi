f486c198f214184a58809f011f58dca6dfc35f13
vitorrrocha <vitorrochaflorip@hotmail.com>
2020-08-17 08:33:43 -0300

    Validation email with yup

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,11 +1,18 @@
 //metodos: index, show, update, store, destroy
 
 import User from '../models/User'
+import * as Yup from 'yup';
 
 class SessionController{
     async store(req, res){
-
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        })
         const { email } = req.body;
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({ error: 'Falha na validação.'});
+        }
 
         let user = await User.findOne({ email });
 
